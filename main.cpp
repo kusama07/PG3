59b27de2a1f225b4c60bffea630ea9bea1042d88
k022g0034@g.neec.ac.jp <k022g0034@g.neec.ac.jp>
2023-10-02 11:09:15 +0900

    01_01完了

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+template<typename Type>
+
+Type min(Type a, Type b) {
+	if (a < b) {
+
+		return a;
+	}
+}
+
+template<>
+
+char min<char>(char a, char b) {
+	return printf("”šˆÈŠO‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+}
+
+int main() {
+	char a = 'a';
+	char b = 'b';
+
+	printf("%d\n", min<int>(2, 4));
+	printf("%f\n", min<float>(100.0f, 200.0f));
+	printf("%lf\n", min<double>(100.12345, 200.12345));
+	printf("%c\n", min<char>(a,b));
+
+	return 0;
+}
