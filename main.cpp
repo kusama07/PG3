0d246c0c9511a72624d23a77294d7d8f341001d5
k022g0034@g.neec.ac.jp <k022g0034@g.neec.ac.jp>
2023-10-02 12:53:43 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,29 +1,2 @@
 ﻿#include <stdio.h>
 
-int Recursive(int n) {
-
-	if (n <= 1) {
-		return (100);
-	}
-
-	return (Recursive(n - 1) * 2 - 50);
-}
-
-int General(int n) {
-
-	return (n * 1072);
-}
-
-int main() {
-	int n = 12;
-	int result;
-	int generalResult;
-
-	result = Recursive(n);
-	generalResult = General(n);
-
-	printf("一般的:%d時間働いたら = %d\n", n, generalResult);
-	printf("再帰的:%d時間働いたら = %d\n", n, result);
-
-	return(0);
-}
