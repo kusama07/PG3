6d3f64df31a3b6bf9706a7c470d55b92184d64c2
k022g0034@g.neec.ac.jp <k022g0034@g.neec.ac.jp>
2023-11-20 07:54:36 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -6,64 +6,67 @@
 #include <string>
 
 typedef std::function<void(int*)> PFunc;
+typedef std::function<void(bool*)> DiceFunc;
 
 void CorrectGuess(int* s) {
-	printf("当たっている\n");
+    printf("当たっている\n");
 }
 
 void MissedGuess(int* s) {
-	printf("間違っている\n");
+    printf("間違っている\n");
 }
 
 void setTimeout(PFunc p, int second) {
-	Sleep(second * 1000);
-	int timeInSeconds = second;
-	p(&timeInSeconds);
+    Sleep(second * 1000);
+    int timeInSeconds = second;
+    p(&timeInSeconds);
 }
 
-void dice(std::function<void(bool*)> s) {
-	char diceChecker[10];
-	scanf_s("%s", &diceChecker, sizeof(diceChecker));
+void dice(DiceFunc s) {
+    char diceChecker[10];
+    scanf_s("%s", &diceChecker, sizeof(diceChecker));
 
-	int diceNum;
-	diceNum = rand() % 6 + 1;
+    int diceNum;
+    diceNum = rand() % 6 + 1;
 
-	bool ansFlag;
+    bool ansFlag;
 
-	if (((diceNum % 2 == 0) && strcmp(diceChecker, "偶数") == 0) ||
-		((diceNum % 2 == 1) && strcmp(diceChecker, "奇数") == 0)) {
-		ansFlag = true;
-	}
-	else {
-		ansFlag = false;
-	}
+    if (((diceNum % 2 == 0) && strcmp(diceChecker, "偶数") == 0) ||
+        ((diceNum % 2 == 1) && strcmp(diceChecker, "奇数") == 0)) {
+        ansFlag = true;
+    }
+    else {
+        ansFlag = false;
+    }
 
-	printf("出目は%dデス", diceNum);
+    printf("出目は%dデス\n", diceNum);
 
-	s(&ansFlag);
+    s(&ansFlag);
 }
 
 int main() {
-	PFunc rightAns;
-	PFunc missedAns;
+    PFunc rightAns;
+    PFunc missedAns;
 
-	bool ansFlag;
+    bool ansFlag;
 
-	rightAns = CorrectGuess;
-	missedAns = MissedGuess;
+    rightAns = CorrectGuess;
+    missedAns = MissedGuess;
 
-	srand(time(0));
+    srand(time(0));
 
-	printf("サイコロを振るので、奇数,偶数を予想し入力してください。\n");
+    printf("サイコロを振るので、奇数,偶数を予想し入力してください。\n");
 
-	dice([&](bool* flag) {
-		if (*flag) {
-			setTimeout(rightAns, 3);
-		}
-		else {
-			setTimeout(missedAns, 3);
-		}
-		});
+    DiceFunc diceCallback = [&](bool* flag) {
+        if (*flag) {
+            setTimeout(rightAns, 3);
+        }
+        else {
+            setTimeout(missedAns, 3);
+        }
+    };
 
-	return 0;
+    dice(diceCallback);
+
+    return 0;
 }
